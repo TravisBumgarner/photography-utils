357c2e0a6bd09e063f1378eacdae03252752ca0d
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-05-29 20:58:15 -0600

    completed algorithm

--- algorithm_explorations/lrtags.ts
+++ algorithm_explorations/lrtags.ts
@@ -3,45 +3,23 @@ import * as exifr from 'exifr';
 type Sidecar = {
   lr: {
     hierarchicalSubject: string[]
+  },
+  dc: {
+    title: {
+      value: string
+    },
+    description: {
+      value: string
+    }
   }
 }
 
-// const processHierarchicalSubject = (hierarchicalSubject: Sidecar['lr']['hierarchicalSubject'] | undefined): undefined => {
-//   // This function will cause failures further down the line with the lie of `as Record<LightroomKey, string>
-//   // In that case need to update Lightroom's Metadata
 
-//   console.log(hierarchicalSubject)
-
-//   // const partialKeys = hierarchicalSubject
-//   //   .reduce((accum, entry) => {
-//   //     const [key, value] = entry.split('|') as [string, string] // gahhhh.
-//   //     accum[key] = value
-//   //     return accum
-//   //   }, {} as Record<string, string>)
-
-//   // return {
-//   //   ...partialKeys,
-//   // } as LightroomMetadata
-// }
-
-
-function collapseToRecord(arr: string[]): Record<string, string[] | true> {
-  return arr.reduce((acc: Record<string, string[]>, str: string) => {
-    // Skip if not hierarchical
-    if (!str.includes('|')) {
-      console.log('skipping tag', str)
-      return acc;
-    }
-
-    const [key, value] = str.split('|');
-
-    if (!value) {
-      acc[key] = acc[key] || [];
-    } else {
-      acc[key] = acc[key] ? [...acc[key], value] : [value];
-    }
-    return acc;
-  }, {});
+const collapsedcRecord = (dc: Sidecar['dc']) => {
+  return {
+    title: dc.title.value,
+    description: dc.description.value
+  }
 }
 
 
@@ -49,18 +27,19 @@ const processPhoto = async (file: string) => {
 
   const sidecar = await exifr.sidecar(file) as unknown as Sidecar
   console.log('sidecar', sidecar)
-  const collapsed = collapseToRecord(sidecar.lr.hierarchicalSubject)
+  const collapsedDc = collapsedcRecord(sidecar.dc)
+  const collapsed = { tags: sidecar.lr.hierarchicalSubject, ...collapsedDc }
   console.log(collapsed)
 
-  // const data = await exifr.parse(file)
-  // console.log('data', data)
+  const data = await exifr.parse(file)
+  console.log('data', data)
 
   // processHierarchicalSubject(sidecar.lr.hierarchicalSubject)
 
 }
 
 const main = async () => {
-  const result = await processPhoto('./h.jpg')
+  const result = await processPhoto('./j.jpg')
   console.log(result)
 }
 
---
+++ algorithm_explorations/template.ts
@@ -0,0 +1,65 @@
+
+
+const lrTags = {
+  tags: [
+    'Location|Mexico',
+    'Donot|exist'
+  ],
+  title: 'caasd',
+  description: 'ascascasc'
+} as const
+
+const photoDetails = {
+  // src: './a.jpg',
+  camera: 'Nikon Z5',
+  lens: 'NIKKOR Z 24-120mm f/4 S',
+  iso: 'ISO 3600',
+  shutterSpeed: '1/80s',
+  aperture: 'ƒ/4.0',
+  focalLength: '120mm',
+  dateTaken: 'October 2022'
+} as const
+
+
+enum ELocation {
+  Mexico = 'Location|Mexico',
+}
+
+const ValidLrTag = {
+  "Location": ELocation
+}
+
+const getTags = (lrTag: string) => {
+  const lookup: Record<string, { tag: string[], account: string[] }> = {
+    [ValidLrTag.Location.Mexico]: {
+      tag: [
+        "abc",
+        "def",
+        "hij",
+      ],
+      account: [
+        "me",
+        "you"
+      ]
+    }
+  }
+
+  if (!lookup[lrTag]) throw Error(`invalid tag: ${lrTag}`)
+
+  const tags = lookup[lrTag]['tag'].map(tag => `#${tag}`).join(' ')
+  const accounts = lookup[lrTag]['account'].map(account => `@${account}`).join(' ')
+  return `${tags} ${accounts}`
+}
+
+let output = ''
+output += `${lrTags.title} ${photoDetails.dateTaken}\n`
+output += `\n`
+output += `${lrTags.description}\n`
+output += `\n`
+output += `The Gear - ${photoDetails.camera}, ${photoDetails.lens}\n`
+output += `The Setup - ${photoDetails.shutterSpeed}, ${photoDetails.aperture}, ${photoDetails.aperture}, ${photoDetails.focalLength} focal length\n`
+output += `\n`
+output += `${getTags(lrTags.tags[0])}\n`
+output += `${getTags(lrTags.tags[1])}\n`
+
+console.log(output)
